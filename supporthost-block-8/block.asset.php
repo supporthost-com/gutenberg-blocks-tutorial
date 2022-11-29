b41a1994e61a53a7efc4cb753ac27e5a5349395a
SupportHost.com <110147707+supporthost-com@users.noreply.github.com>
2022-11-29 11:12:14 -0500

    Add files via upload

---
+++ supporthost-block-8/block.asset.php
@@ -0,0 +1,11 @@
+<?php return
+    array( 'dependencies' =>
+        array(
+            'wp-blocks',
+            'wp-element',
+            'wp-block-editor',
+            'wp-polyfill',
+            'wp-server-side-render'
+        ),
+        'version' => '0.1'
+    );
